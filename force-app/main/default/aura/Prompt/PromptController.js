f1446844624aed21b43e5ef5435712761833ed69
Carlos Pintor <carlospintor.sfdc@gmail.com>
2020-05-24 18:42:37 -0500

    Prompt markup updated and functions implemented

--- force-app/main/default/aura/Prompt/PromptController.js
+++ force-app/main/default/aura/Prompt/PromptController.js
@@ -1,5 +1,12 @@
 ({
-    myAction : function(component, event, helper) {
+    onClick : function(component, event, helper) {
+        component.set('v.showThis', false);
+    },
 
+    onShow : function(component, event, helper) {
+        var params = event.getParam('arguments');
+        component.set('v.title', params.title);
+        component.set('v.messageList', params.messageList);
+        component.set('v.showThis', true);
     }
 })
